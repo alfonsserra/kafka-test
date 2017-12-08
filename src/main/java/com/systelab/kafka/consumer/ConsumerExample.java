c9b003daeda79c3b33c364c625af2232c20259df
alfonsserra <alfonsoserra@gmail.com>
2017-12-08 18:31:47 +0100

    Add Kafka consumer configuration options. Do the Consumer with thread extend the simple consumer.

--- src/main/java/com/systelab/kafka/consumer/ConsumerExample.java
+++ src/main/java/com/systelab/kafka/consumer/ConsumerExample.java
@@ -1,14 +1,13 @@
 package com.systelab.kafka.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
 
 public class ConsumerExample {
@@ -22,14 +21,31 @@ public class ConsumerExample {
         configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        configProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true"); //default value. Do not commit the messages manually
+        configProperties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 5000); //default value
+        configProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); //default value. Other: earliest and none
+
         configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
         return configProperties;
     }
 
+    public KafkaConsumer<String, String> getKafkaConsummer(String topic, String group) {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(getKafkaProperties(group));
+        consumer.subscribe(Arrays.asList(topic), new ConsumerRebalanceListener() {
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                logger.info(Arrays.toString(partitions.toArray()) + " topic-partitions are revoked from this consumer\n");
+            }
+
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                logger.info(Arrays.toString(partitions.toArray()) + " topic-partitions are assigned to this consumer\n");
+            }
+        });
+        return consumer;
+    }
+
     public void consume() {
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(getKafkaProperties(ConsumerExample.GROUP));
-        consumer.subscribe(Arrays.asList(ConsumerExample.TOPIC));
+        KafkaConsumer<String, String> consumer = getKafkaConsummer(ConsumerExample.TOPIC, ConsumerExample.GROUP);
         try {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(100);
--- src/main/java/com/systelab/kafka/consumer/ConsumerWithThreadExample.java
+++ src/main/java/com/systelab/kafka/consumer/ConsumerWithThreadExample.java
@@ -1,62 +1,26 @@
 package com.systelab.kafka.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.Properties;
 import java.util.Scanner;
 
-
-public class ConsumerWithThreadExample {
+public class ConsumerWithThreadExample extends ConsumerExample {
 
     private static final Logger logger = LogManager.getLogger(ConsumerWithThreadExample.class);
 
-    public static String TOPIC = "modulab";
-    public static String GROUP = "21";
-
-    public static void main(String[] argv) throws Exception {
-
-        Scanner in = new Scanner(System.in);
-        ConsumerThread consumerRunnable = new ConsumerThread(ConsumerWithThreadExample.TOPIC, ConsumerWithThreadExample.GROUP);
-        consumerRunnable.start();
-        String line = "";
-        while (!line.equals("exit")) {
-            line = in.next();
-        }
-        consumerRunnable.getKafkaConsumer().wakeup();
-        logger.info("Stopping consumer ...");
-        consumerRunnable.join();
-    }
-
-    private static class ConsumerThread extends Thread {
-        private String topicName;
-        private String groupId;
+    private class ConsumerThread extends Thread {
         private KafkaConsumer<String, String> consumer;
 
-        public ConsumerThread(String topicName, String groupId) {
-            this.topicName = topicName;
-            this.groupId = groupId;
+        public ConsumerThread() {
         }
 
-        protected Properties getKafkaProperties(String groupId) {
-            Properties configProperties = new Properties();
-            configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-            configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-            configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-            configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-            configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
-            return configProperties;
-        }
         public void run() {
-            consumer = new KafkaConsumer<String, String>(getKafkaProperties(groupId));
-            consumer.subscribe(Arrays.asList(topicName));
+            consumer = getKafkaConsummer(ConsumerExample.TOPIC, ConsumerExample.GROUP);
             try {
                 while (true) {
                     ConsumerRecords<String, String> records = consumer.poll(100);
@@ -64,15 +28,40 @@ public class ConsumerWithThreadExample {
                         logger.info("Message partition->" + record.partition() + " stored at offset->" + record.offset() + ": " + record.value());
                 }
             } catch (WakeupException ex) {
-                logger.error("Exception caught", ex);
+                logger.error("Wakeup requested");
             } finally {
                 consumer.close();
             }
+        }
+
+        public void wakeup() {
+            consumer.wakeup();
+
         }
 
         public KafkaConsumer<String, String> getKafkaConsumer() {
             return this.consumer;
         }
     }
-}
 
+    public void consume() {
+        try {
+            Scanner in = new Scanner(System.in);
+            ConsumerThread consumerRunnable = new ConsumerThread();
+            consumerRunnable.start();
+            String line = "";
+            while (!line.equals("exit")) {
+                line = in.next();
+            }
+            logger.info("Stopping consumer ...");
+            consumerRunnable.wakeup();
+            consumerRunnable.join();
+        } catch (Exception ex) {
+            logger.error("Exception caught", ex);
+        }
+    }
+
+    public static void main(String[] argv) throws Exception {
+        new ConsumerWithThreadExample().consume();
+    }
+}
